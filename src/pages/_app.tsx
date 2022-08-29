7a241d85b4ec7cd2324d6d7be9986977b6a0ddaf
Milene Taborda <milenetaborda1@outlook.com>
2022-08-29 11:32:00 -0300

    chore: adicionando next-seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Fragment, useEffect } from "react";
@@ -11,12 +12,28 @@ function App({ Component, pageProps }: AppProps) {
     if (typeof window === "undefined") return undefined;
   }, []);
 
+  const seoDescription =
+    "Acreditamos que aqui você encontra o melhor da gastronomia do Brasil, como nossos pratos e bebidas. Venha conhecer nossos restaurantes.";
+
   return (
     <Fragment>
       <Head>
         <title>Restaurant app</title>
-        <meta name="description" content="Restaurant app" />
+        <meta name="description" content={seoDescription} />
         <link rel="icon" href="/favicon.ico" />
+
+        <NextSeo
+          title={"Restaurant app"}
+          description={seoDescription}
+          openGraph={{
+            title: "Restaurant app",
+            description: seoDescription,
+            locale: "pt_BR",
+            site_name: "Restaurant app",
+            url: `https://main.d2r4yumkch0add.amplifyapp.com/`,
+            type: "website",
+          }}
+        />
       </Head>
       <GlobalStyles />
       <ThemeProvider theme={defaultTheme}>
